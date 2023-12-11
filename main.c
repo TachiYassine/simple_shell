39206ac8002c4c501744c080b589b24497257ed9
TachiYassine <tachi.yassine.3@gmail.com>
2023-12-11 13:18:23 -0800

    Principal function, our main

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - simple shell main function that run all other functions
+ * @ac: count of arguments
+ * @av: arguments
+ * 
+ * Return: 0 Always (success).
+ */
+
+int main(int ac, char **av)
+{
+	char *line = NULL;
+
+}
