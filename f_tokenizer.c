9c70927ee90a4790bd250141b0ff864ba0fac5b0
TachiYassine <tachi.yassine.3@gmail.com>
2023-12-12 01:08:55 -0800

    Update

---
+++ f_tokenizer.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+
+/**
+ * tokenizer - .........
+ * @line: input from user .....
+ *
+ * Return .....
+ */
+
+char **tokenizer(char *line)
+{
+	char *token = NULL;
+	char *tmp_line = NULL;
+	char *command = NULL;
+	int cpt_str = 0, i = 0;
+
+	if (line == NULL)
+		return (NULL);
+	tmp_line = _Str_duplicate(line);
+
+	token = strtok(tmp_line, DELIMITER);
+	if (token == NULL)
+	{
+		free(line), line = NULL;
+		free(tmp_line), tmp_line = NULL;
+		return (NULL);
+	}
+
+	while (token)
+	{
+		cpt_str++;
+		token = strtok(NULL, DELIMITER);
+	}
+	free(tmp_line), tmp_line = NULL;
+
+	command = malloc(sizeof(char *) * (cpt_str + 1));
+	if (command == NULL)
+	{
+		free(line), line = NULL;
+		return (NULL);
+	}
+
+	token = strtok(line, DELIMITER);
+	while (token)
+	{
+		command[i] = _Str_duplicate(token);
+		token = strtok(NULL, DELIMITER);
+		i++;
+	}
+	free(line), line = NULL;
+	command[i] = NULL;
+	return (command);
+}
---
+++ read_from_user.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/*
+ * read_from_user - .......update this description later
+ * @line: ......
+ *
+ * Return string
+ */
+
+char *read_from_user(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t n;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+
+	n = getline(&line, &len, stdin);
+	if (n == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+
+	return (line);
+}
