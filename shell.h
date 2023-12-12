d69c8dcc3ee4a4e35e5077d4d2a409ef07eb25e4
TachiYassine <tachi.yassine.3@gmail.com>
2023-12-12 01:06:32 -0800

    Create our headers file -update-

--- shell.h
+++ shell.h
@@ -1,6 +1,8 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#define DELIMITER " \t\n"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,6 +13,13 @@
 #include <errno.h>
 #include <fcntl.h>
 
+char *read_from_user(void);
+char **tokenizer(char *line);
 
+char *_Str_duplicate(const char *str);
+int _str_compare(char *str1, char *str2);
+int _strlen(char *s);
+char * _strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
 #endif
