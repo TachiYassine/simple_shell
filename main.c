d4689dcf8143ff9a08c279e1ecc203ce2d7a9b8c
TachiYassine <tachi.yassine.3@gmail.com>
2023-12-12 01:07:08 -0800

    Principal function, our main -update-

--- main.c
+++ main.c
@@ -10,6 +10,34 @@
 
 int main(int ac, char **av)
 {
-	char *line = NULL;
+	char *line = NULL, **command = NULL;
+	int status = 0;
+	(void) ac;
+	(void) av;
 
+	while(1)
+	{
+		line = read_from_user();
+		if (line == NULL) // end of file or ctrl + D
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n" , 1);
+			return (status);
+		}
+
+		command = tokenizer(line);
+		if (command == NULL)
+			continue;
+
+		for (i = 0; command[i]; i++)
+		{
+			printf("%s\n", command[i]);
+			// we are not allow to use printf i will update it later
+			free(command[i]);
+			command[i] = NULL;
+		}
+		free(command), command = NULL;
+
+
+	}
 }
